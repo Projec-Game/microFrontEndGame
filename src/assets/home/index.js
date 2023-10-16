e09fe2fe6c6d6bc246df97a9d3081782d00470e9
julianIPT <julian.acosta@inglesparatodos.edu.co>
2023-10-16 16:38:06 -0500

    Adjust error limited objet person

--- src/assets/home/index.js
+++ src/assets/home/index.js
@@ -1,8 +1,10 @@
 // HomeIcons
 import icon_save from "./complete-registration/icon_save.svg"
+import player from "./complete-registration/player.gif"
 
 
 export const HomeIcons = {
     icon_save,
+    player
 
 }
--- src/components/templates/game-play-template/game-play-template.js
+++ src/components/templates/game-play-template/game-play-template.js
@@ -1,13 +1,15 @@
 import React, { useState, useEffect } from 'react';
 
 const Game = () => {
+  
   const [playerPosition, setPlayerPosition] = useState({ x: window.innerWidth / 2 - 25, y: window.innerHeight - 75 });
   const [obstacles, setObstacles] = useState([]);
   const [score, setScore] = useState(0);
   const [isGameRunning, setIsGameRunning] = useState(true);
   const [maxScore, setMaxScore] = useState(0);
-  const [maxScorePlayer, setMaxScorePlayer] = useState('Jugador Anónimo');
+  // const [maxScorePlayer, setMaxScorePlayer] = useState('Jugador Anónimo');
   const obstacleSpeed = 5;
+  const [playerName] = useState(localStorage.getItem('name'));
 
   useEffect(() => {
     const handleOrientation = (event) => {
@@ -16,10 +18,11 @@ const Game = () => {
         const newX = playerPosition.x - x * 2;
         const minX = 0;
         const maxX = window.innerWidth - 50;
-        const clampedX = Math.min(Math.max(newX, minX), maxX);
+        const clampedX = Math.min(Math.max(newX, minX), maxX - 50);
         setPlayerPosition((prev) => ({ ...prev, x: clampedX }));
       }
     };
+    
 
     if (isGameRunning) {
       window.addEventListener('deviceorientation', handleOrientation, true);
@@ -41,7 +44,9 @@ const Game = () => {
     const moveObstacles = () => {
       if (isGameRunning) {
         setObstacles((prev) =>
-          prev.map((obstacle) => ({ ...obstacle, y: obstacle.y + obstacleSpeed }))
+          prev
+            .map((obstacle) => ({ ...obstacle, y: obstacle.y + obstacleSpeed }))
+            .filter((obstacle) => obstacle.y < window.innerHeight)
         );
       }
     };
@@ -83,7 +88,7 @@ const Game = () => {
         setMaxScore(score);
         // Puedes pedir al jugador que ingrese su nombre aquí y almacenarlo en maxScorePlayer
         // Por ahora, simplemente estableceremos un valor predeterminado
-        setMaxScorePlayer('Jugador Anónimo');
+        // setMaxScorePlayer('Jugador Anónimo');
       }
     }
   }, [isGameRunning, score, maxScore]);
@@ -99,15 +104,17 @@ const Game = () => {
     <div style={{ position: 'relative', height: '100vh' }}>
       <div
         style={{
-          position: 'absolute',
-          left: `${playerPosition.x}px`,
-          top: `${playerPosition.y}px`,
-          width: '50px',
-          height: '50px',
-          background: 'blue',
+        position: 'absolute',
+        left: `${playerPosition.x}px`,
+        top: `${playerPosition.y}px`,
+        width: '100px', // Ajusta el ancho según el tamaño de la imagen
+        height: '100px', // Ajusta la altura según el tamaño de la imagen
+        backgroundImage: `url(${require('./../../../assets/home/complete-registration/player.gif')})`,// Reemplaza 'url_de_la_imagen.gif' con la URL de tu imagen animada
+        backgroundSize: 'cover', // Ajusta el tamaño de la imagen según sea necesario
+        backgroundRepeat: 'no-repeat',
         }}
       >
-        Player
+        {/* {playerName} */}
       </div>
       {obstacles.map((obstacle) => (
         <div
@@ -125,7 +132,7 @@ const Game = () => {
         </div>
       ))}
       <p style={{ position: 'absolute', top: '10px', left: '10px', color: 'white' }}>
-        Score: {score} | Max Score: {maxScore} (Jugador: {maxScorePlayer})
+        Score: {score} | Max Score: {maxScore} (Jugador: {playerName})
       </p>
       {!isGameRunning && (
         <button onClick={resetGame} style={{ position: 'absolute', top: '50px', left: '10px' }}>
